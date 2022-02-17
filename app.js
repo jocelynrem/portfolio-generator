95b26cec9d57983ce52679b9e3a5d1f5bd84b88f
jocelynrem <jocelynrem@gmail.com>
2022-02-17 17:58:36 -0500

    added style sheet

--- app.js
+++ app.js
@@ -128,26 +128,21 @@ const promptProject = portfolioData => {
     })
 };
 
-// promptUser()
-//   .then(promptProject)
-//   .then(portfolioData => {
-//     console.log(portfolioData)
-// const pageHTML = generatePage(name, github)
-
-// fs.writeFile('./index.html', pageHTML, err => {
-//   if (err) throw err
-
-//   console.log('Portfolio complete! Check out index.html to see the output.')
-// })
-
 promptUser()
   .then(promptProject)
   .then(portfolioData => {
-    const pageHTML = generatePage(portfolioData);
-
-    // fs.writeFile('./index.html', pageHTML, err => {
-    //   if (err) throw new Error(err);
-
-    //   console.log('Page created! Check out index.html in this directory to see it!');
-    // });
+    return generatePage(portfolioData);
+  })
+  .then(pageHTML => {
+    return writeFile(pageHTML);
+  })
+  .then(writeFileResponse => {
+    console.log(writeFileResponse);
+    return copyFile();
+  })
+  .then(copyFileResponse => {
+    console.log(copyFileResponse);
+  })
+  .catch(err => {
+    console.log(err);
   });
